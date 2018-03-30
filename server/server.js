4b2334315f9409af6d32435b2a2f20fb3fe21ff6
danielVg <danielveragi@gmail.com>
2018-03-30 17:49:47 +0100

    added debug module
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 const express = require('express');
-var router = require("./routes/routes.js");
 const path = require('path');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const dotenv = require('dotenv').config();
+const debug = require('debug')('APP:server')
 const app = express();
 
-app.use(morgan("combined"));
 
+// morgan logging utility
+app.use(morgan("combined"));
 // specify view engine & static files
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../client"));
 app.use(express.static(path.join(__dirname, "../client")));
+// body parser
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
+// mongoose
+mongoose.connect(process.env.DB_CONN);
 
+// router
+var router = require("./routes/routes.js");
 app.use('/', router);
 
 module.exports = app;
