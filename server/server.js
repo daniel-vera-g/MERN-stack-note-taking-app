f874cf7840d7ae65129aab144e7d6800dbd0f979
danielVg <danielveragi@gmail.com>
2018-03-30 16:52:34 +0100

    added morgan for http logging
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 var router = require("./routes/routes.js");
 const path = require('path');
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan("combined"));
 
 // specify view engine & static files
 app.set("view engine", "ejs");
